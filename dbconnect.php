14ee1b53b30d0d10877f949b2892879be75fd4f4
jocod <itpolishtranslate@outlook.com>
2016-11-03 13:54:47 +0000

    First Commit

---
+++ dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+//code to setup connection to the database
+
+$conn = mysql_connect("localhost","jocod","");
+mysql_select_db("c9");
+session_start();
+
+if (false===$conn){//forces to false or true
+    die("Connection failed");
+}
+    else {
+        echo 'Database connected';
+    
+}
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require('dbconnect.php');//link to the data base .php file
+//setup variable to hold sql statment
+
+if($_SERVER['REQUEST_METHOD']=='POST'|| $SESSION['login']===1){
+    //mysql_escape_string terminates the content of the form box, protecting it from MYSQL injection
+$uname = mysql_escape_string($_POST['namebox']);
+$password = mysql_escape_string($_POST['passwordbox']);
+ 
+$sql=mysql_query("SELECT * FROM users WHERE username='$uname'AND password='$password'");
+$numberrows = mysql_num_rows($sql);
+//if login succesful will display sweets
+$_SESSION['login']=1;
+if ($numberrows>0) {
+    //login has been succesfull so display the sweets
+    $sweets = "SELECT * FROM sweets";
+    $result = mysql_query($sweets);
+    
+    while($row=mysql_fetch_assoc($result)){
+        echo $row["name"];
+    }
+}
+else {
+  echo'<p>Login failed.</p>';
+  
+  }
+  echo'
+<form method="POST" action="index.php">
+    Username <input type="text" name="namebox"><br/>
+    Password <input type="text" name="passwordbox"><br/>
+    <input type="submit" name="submit" value="Log in">
+</form>';
+}
+//close if statment after pressing submit form button
+?>
